6e43a2634f3c43b0b6c2680d4f2949dc16b483e0
gildobro <gdobrovinsky@gmail.com>
2020-09-02 16:28:59 -0400

    Some more changes

--- Basics/Functions.py
+++ Basics/Functions.py
@@ -1,4 +1,4 @@
-'''
+
 #the len() method outputs the amount of characters that a string has
 #can only take one argument
 print(len('stefonapodopoulous'))
@@ -10,7 +10,7 @@ print(len('stefonapodopoulous'))
 print(isinstance(11,int))
 print(isinstance(13, float))
 print(isinstance("hello", str))
-print(isinstance(1,3,int))
+#print(isinstance(1,3,int))
 #output1 = True
 #output2 = False
 #output3 = True
@@ -21,7 +21,7 @@ def mean(*args):
     print(len(args))
     return sum(args) / len(args)
 print(mean(1,3, 7, 5.3, 12))
-
+'''
 #keyword arguments
 def mean(**kwargs):
     return kwargs
--- Basics/listComprehension.py
+++ Basics/listComprehension.py
@@ -1,10 +1,12 @@
 temps = [221, 234, 340, -9999, 230]
 
+'''
+#conditional list with if/else (order matters)
 new_temps = [temp / 10 if temp != -9999 else 0 for temp in temps]
 print(new_temps)
 
-'''
-#conditional list
+
+#conditional list with an if
 new_temps = [temp / 10 for temp in temps if temp != -9999]
 
 print(new_temps)
